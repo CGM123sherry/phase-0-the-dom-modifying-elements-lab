4c16ccd9f393f83325c06dd26e8e35012878b8db
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-06 12:58:55 +0300

    Modified the dom elements

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// // Write your code here!
+// const newHeader = document.createElement('h1');
+// document.body.append(newHeader);
+// newHeader.textContent = "Sherry is the champion";
+// document.body.appendChild(newHeader);
+
+// const mainElement = document.getElementById('main');
+// if (mainElement) {
+//     mainElement.remove();
+// }
+document.querySelector('main#main').remove();
+//creating champion name;
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Sherry is the champion';
